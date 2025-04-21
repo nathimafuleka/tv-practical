d3aee6ee80a5c1a1885c8c03678eae7ebc9e8b34
Nathi Mafuleka <nbn.mafuleka@gmail.com>
2025-04-21 15:51:48 +0200

    upload changes

--- tv-parctical-fe-starter/fibertime_fe/next.config.js
+++ tv-parctical-fe-starter/fibertime_fe/next.config.js
@@ -1,33 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ],
-      },
-    ];
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true
   },
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://tv-practical-api.vercel.app/api/:path*',
-      },
-    ];
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  output: 'standalone',
-};
+        destination: 'https://tv-practical-api.vercel.app/api/:path*'
+      }
+    ]
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
